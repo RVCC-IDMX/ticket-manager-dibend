7af6d7972b2c8354df51aec1fea590e07c9ac692
David DiBenedetto <david@njweb.solutions>
2022-10-24 17:52:35 -0400

    Finished program
    Finished event listener and chalk ticket program

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const TicketManager = require('./ticketManager');
 const EmailService = require('./emailService');
 const DatabaseService = require('./databaseService');
@@ -6,12 +7,31 @@ const ticketManager = new TicketManager(3);
 const emailService = new EmailService();
 const databaseService = new DatabaseService();
 
+// eslint-disable-next-line no-console
+const { log } = console;
+
 ticketManager.on('buy', (email, price, timestamp) => {
   emailService.send(email);
   databaseService.save(email, price, timestamp);
 });
 
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
-ticketManager.buy('test@email.com', 10);
+ticketManager.on('error', (error) => {
+  // eslint-disable-next-line no-console
+  console.error(chalk.yellow(`Gracefully handling our error: ${error}`));
+});
+
+log(`We have ${ticketManager.listenerCount('buy')} listener(s) for the buy event`);
+log(`We have ${ticketManager.listenerCount('error')} listener(s) for the error event`);
+
+const onBuy = () => {
+  log(chalk.cyanBright('I will be removed soon'));
+};
+
+ticketManager.on('buy', onBuy);
+log(chalk.blue.bgRed.bold(`We added a new event listener bringing our total count for the buy event to: ${ticketManager.listenerCount('buy')}`));
+ticketManager.buy('test@email', 20);
+
+ticketManager.removeAllListeners('buy');
+log(`We have ${ticketManager.listenerCount('buy')} listeners for the buy event`);
+ticketManager.buy('test@email', 20);
+log(chalk.red.bgBlue('The last ticket was bought'));
