9b7e541119f78618c6de63b22cb30bcd7346d92b
David DiBenedetto <david@njweb.solutions>
2022-10-24 17:30:56 -0400

    Fix everything
    Get js out of .vscode

---
+++ databaseService.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+
+const { log } = console;
+
+class DatabaseService {
+  // eslint-disable-next-line class-methods-use-this
+  save(email, price, timestamp) {
+    log(chalk.red(`Running query: INSERT INTO orders VALUES (email, price, created) VALUES (${email}, ${price}, ${timestamp})`));
+  }
+}
+
+module.exports = DatabaseService;
---
+++ emailService.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+
+const { log } = console;
+
+class EmailService {
+  // eslint-disable-next-line class-methods-use-this
+  send(email) {
+    log(chalk.green(`Sending email to ${email}`));
+  }
+}
+
+module.exports = EmailService;
